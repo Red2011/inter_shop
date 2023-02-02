5a139baef83180f3424967b7c2b9b33cff25bd12
Red2011 <ivan.shlendin@mail.ru>
2023-02-02 20:45:56 +1000

    added new test's

--- components/tests/func.test.tsx
+++ components/tests/func.test.tsx
@@ -1,6 +1,7 @@
 import {describe, test, expect} from "@jest/globals";
 import {result} from "../../pages";
-import {prevSlide, nextSlide} from "../../pages/products/[id]";
+import {prevSlide, nextSlide, init_hiddens} from "../../pages/products/[id]";
+import {default_address, SetAddress} from "../helpers/setAddres";
 
 describe('testing result', () =>{
     test('normal', ()=>{
@@ -9,19 +10,37 @@ describe('testing result', () =>{
         expect(result(234)).toBe("object-left")
     })
     test('prevSlide', () => {
-        const slide = ["a", "b", "c", "d"]
-        expect(prevSlide(2, slide)).toBe(1)
-        expect(prevSlide(3, slide)).toBe(2)
-        expect(prevSlide(0, slide)).toBe(3)
-        expect(prevSlide(113, slide)).toBe(0)
+        let slide = ["hidden", "hidden", "block", "hidden"]
+        slide = prevSlide(slide)
+        expect(slide).toEqual(["hidden", "block", "hidden", "hidden"])
+        slide = prevSlide(slide)
+        slide = prevSlide(slide)
+        expect(slide).toEqual(["hidden", "hidden", "hidden", "block"])
     })
     test('nextSlide', () => {
-        const slide = ["a", "b", "c", "d"]
-        expect(nextSlide(2, slide)).toBe(3)
-        expect(nextSlide(3, slide)).toBe(0)
-        expect(nextSlide(0, slide)).toBe(1)
-        expect(nextSlide(113, slide)).toBe(0)
+        let slide = ["hidden", "hidden", "block", "hidden"]
+        slide = nextSlide(slide)
+        expect(slide).toEqual(["hidden", "hidden", "hidden", "block"])
+        slide = nextSlide(slide)
+        slide = nextSlide(slide)
+        expect(slide).toEqual(["hidden", "block", "hidden", "hidden"])
     })
+    test('initialize array', () =>{
+        expect(init_hiddens(0)).toEqual(["block"])
+        expect(init_hiddens(1)).toEqual(["block"])
+        expect(init_hiddens(2)).toEqual(["block", "hidden"])
+        expect(init_hiddens(23)).toEqual(["block", "hidden", "hidden", "hidden", "hidden",
+            "hidden", "hidden", "hidden", "hidden", "hidden", "hidden", "hidden", "hidden", "hidden",
+            "hidden", "hidden", "hidden", "hidden", "hidden", "hidden", "hidden", "hidden", "hidden"])
+    })
+    test('setAddress', () => {
+        expect(default_address).toEqual("https://dummyjson.com/products")
+        SetAddress("another url")
+        expect(default_address).toEqual("another url")
+        SetAddress("")
+        expect(default_address).toEqual("another url")
+    })
+
 })
 
 
