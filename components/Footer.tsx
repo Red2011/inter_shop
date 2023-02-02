d13a52a7824b2e648d4ebf3ae6f23013a6d9fa94
Red2011 <ivan.shlendin@mail.ru>
2023-02-02 20:42:15 +1000

    added text size for footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <footer>
-            <div className="flex justify-between font-bold mx-5 mb-2">
+            <div className="flex justify-between font-bold mx-5 mb-2 max-[590px]:text-[12px]">
                 <div>Inspired by: DummyJSON</div>
                 <div>InterShop©</div>
             </div>
